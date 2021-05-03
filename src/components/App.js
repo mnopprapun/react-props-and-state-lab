8cfd28abfe14afb78c9b626feb05d63e245cfe42
Marc Nopprapun <mnopprapun@gmail.com>
2021-05-03 13:23:04 -0500

    commit tree fe0742076260b1e3795fbe22d5d5fa006957ccb0

--- src/components/App.js
+++ src/components/App.js
@@ -10,10 +10,37 @@ class App extends React.Component {
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
+        type: 'all', 
       }
     }
   }
+
+
+  // i consoled loged to i can see the information to use first
+  // lab requires to select specific data to show such as cat and dog first when the option is selected
+  onFindPetsClick = () => {
+    let url = "/api/pets"
+    if (this.state.filters.type !== 'all') {
+      url += `?type=${this.state.filters.type}`
+    }
+
+    fetch(url)
+      .then(res => res.json())
+      .then(pets => this.setState({ pets: pets}))
+  }
+  // .then(pets => console.log(pets))}
+
+  // function for option change pet type
+  onChangeType = ({ target: { value } }) => {
+    this.setState({ filters: { ...this.state.filters, type: value } })
+  }
+
+onAdoptPet = petId => {
+  let pets = this.state.pets.map(p => {
+    return p.id === petId ? { ...p, isAdopted: true } : p;
+  });
+  this.setState({pets: pets});
+}
 
   render() {
     return (
@@ -24,10 +51,12 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onFindPetsClick={this.onFindPetsClick}
+                onChangeType={this.onChangeType} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
